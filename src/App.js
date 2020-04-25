84b45315ed6d5700a9a71ab0e55d7627c01a2486
camiilavillamizar <camiilavillamizar@gmail.com>
2020-04-24 21:12:46 -0500

    correcciones

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ function App() {
   return (
     <div  className= "container">
       <div className = "frase">
-        <h1>"La risa no tiene tiempo, la imaginación no tiene edad
-          y los sueños son para siempre"
-        </h1>
+        <div className = "line">
+            <p id="la"> La </p>
+            <h1>RISA</h1>
+        </div>
+       
+        <p id="notiene">no tiene </p>
+        <h1 id="tiempo">TIEMPO</h1>
+        <p id="la2">la </p>
+        <h1 id = "imaginacion">IMAGINACIÓN</h1>         
+        <p id="notiene2">no tiene</p>
+        <h1 id = "edad">EDAD</h1>
+        <p id="y">y los</p>
+        <h1 id = "suenos">SUEÑOS</h1>
+        
+        <p id="sonpara">son para</p>
+        <h1 id="siempre">SIEMPRE</h1>
       </div>
 
       <div className = "autor">
-        <h1>- Walt Disney</h1>
+        <h1>-Walt Disney</h1>
       </div>
     </div>
   );
